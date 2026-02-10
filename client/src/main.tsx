7e533525d7a3d9e1e0faa6589b04ae4b801c13eb
Arvin Buid <arvinbuid01@gmail.com>
2026-02-10 19:38:18 +0800

    feat: create themeSlice and workspaceSlice redux slices, add two slice reducers to store and provide the redux store to react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './app/store.ts'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 )
