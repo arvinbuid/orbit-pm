4e9a81b8c2ee1f3a52f9f449e625291a8512d3ce
Arvin Buid <arvinbuid01@gmail.com>
2026-02-18 11:13:26 +0800

    style: fix and move project info section inside task info div wrapper

--- client/src/pages/TaskDetails.tsx
+++ client/src/pages/TaskDetails.tsx
@@ -150,21 +150,21 @@ const TaskDetails = () => {
                         </div>
                     </div>
                 </div>
-            </div>
 
-            {/* Project Info */}
-            {project && (
-                <div className="p-4 rounded-md bg-white dark:bg-zinc-900 text-zinc-700 dark:text-zinc-200 border border-gray-300 dark:border-zinc-800 ">
-                    <p className="text-xl font-medium mb-4">Project Details</p>
-                    <h2 className="text-gray-900 dark:text-zinc-100 flex items-center gap-2"> <PenIcon className="size-4" /> {project.name}</h2>
-                    <p className="text-xs mt-3">Project Start Date: {format(new Date(project.start_date), "dd MMM yyyy")}</p>
-                    <div className="flex flex-wrap gap-2 text-sm text-gray-500 dark:text-zinc-400 mt-4">
-                        <span>Status: {project.status}</span>
-                        <span>Priority: {project.priority}</span>
-                        <span>Progress: {project.progress}%</span>
+                {/* Project Info */}
+                {project && (
+                    <div className="p-4 rounded-md bg-white dark:bg-zinc-900 text-zinc-700 dark:text-zinc-200 border border-gray-300 dark:border-zinc-800 ">
+                        <p className="text-xl font-medium mb-4">Project Details</p>
+                        <h2 className="text-gray-900 dark:text-zinc-100 flex items-center gap-2"> <PenIcon className="size-4" /> {project.name}</h2>
+                        <p className="text-xs mt-3">Project Start Date: {format(new Date(project.start_date), "dd MMM yyyy")}</p>
+                        <div className="flex flex-wrap gap-2 text-sm text-gray-500 dark:text-zinc-400 mt-4">
+                            <span>Status: {project.status}</span>
+                            <span>Priority: {project.priority}</span>
+                            <span>Progress: {project.progress}%</span>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
