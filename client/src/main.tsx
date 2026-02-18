68732a57618bc08b79717b42d8c29caa93862c1e
Arvin Buid <arvinbuid01@gmail.com>
2026-02-19 01:52:10 +0800

    feat: setup clerk provider in main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,25 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
+import { StrictMode } from 'react'
+import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.tsx'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ClerkProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
