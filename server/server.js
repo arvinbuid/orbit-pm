a9aff0d53672b18b98d53ab50c8c4688edfb32e3
Arvin Buid <arvinbuid01@gmail.com>
2026-02-19 18:58:55 +0800

    feat: install inngest, setup client and http endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import {clerkMiddleware} from "@clerk/express";
+import {serve} from "inngest/express";
+import {inngest, functions} from "./inngest/index.js";
 
 const app = express();
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(clerkMiddleware());
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({client: inngest, functions}));
 
 const PORT = process.env.PORT || 5001;
 
