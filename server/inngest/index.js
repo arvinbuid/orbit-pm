4e168429e938b69a576261c5484c032d8a1aae3f
Arvin Buid <arvinbuid01@gmail.com>
2026-02-19 19:47:39 +0800

    fix: initialize prisma client on inngest client file

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
-import {PrismaClient as prisma} from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
 import {Inngest} from "inngest";
+
+const prisma = new PrismaClient();
 
 // Create a client to send and receive events
 export const inngest = new Inngest({id: "project-management-lenis"});
