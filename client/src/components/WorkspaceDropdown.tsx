1540ce427111ba7ba72e414f6318f545fd656d4f
Arvin Buid <arvinbuid01@gmail.com>
2026-02-23 20:32:13 +0800

    feat: replace dummy workplace data with clerk organization data in WorkspaceDropdown component

--- client/src/components/WorkspaceDropdown.tsx
+++ client/src/components/WorkspaceDropdown.tsx
@@ -3,19 +3,24 @@ import { ChevronDown, Check, Plus } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { setCurrentWorkspace } from "../features/workspaceSlice";
 import { useNavigate } from "react-router-dom";
-import { dummyWorkspaces } from "../assets/assets";
 import { useAppSelector } from "../app/hooks";
+import { useClerk, useOrganizationList } from "@clerk/clerk-react";
 
 function WorkspaceDropdown() {
     const [isOpen, setIsOpen] = useState(false);
     const dropdownRef = useRef<HTMLDivElement>(null);
     const { workspaces } = useAppSelector((state) => state.workspace);
-    const currentWorkspace = useAppSelector((state) => state.workspace?.currentWorkspace || null);
+    const { currentWorkspace } = useAppSelector((state) => state.workspace);
+    const { setActive, userMemberships, isLoaded } = useOrganizationList({ userMemberships: true });
+    const { openCreateOrganization } = useClerk();
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const onSelectWorkspace = (organizationId: string) => {
+        if (!setActive) return;
+        setActive({ organization: organizationId });
+        console.log(organizationId)
         dispatch(setCurrentWorkspace(organizationId))
         setIsOpen(false);
         navigate('/')
@@ -32,11 +37,20 @@ function WorkspaceDropdown() {
         return () => document.removeEventListener("mousedown", handleClickOutside);
     }, []);
 
+    useEffect(() => {
+        if (currentWorkspace && isLoaded) {
+            setActive({ organization: currentWorkspace.id });
+        }
+    }, [currentWorkspace, isLoaded, setActive]);
+
     return (
         <div className="relative m-4" ref={dropdownRef}>
-            <button onClick={() => setIsOpen(prev => !prev)} className="w-full flex items-center justify-between p-3 h-auto text-left rounded hover:bg-gray-100 dark:hover:bg-zinc-800" >
+            <button
+                onClick={() => setIsOpen(prev => !prev)}
+                className="w-full flex items-center justify-between p-3 h-auto text-left rounded hover:bg-gray-100 dark:hover:bg-zinc-800"
+            >
                 <div className="flex items-center gap-3">
-                    <img src={currentWorkspace.image_url} alt={currentWorkspace.name} className="w-8 h-8 rounded shadow" />
+                    <img src={currentWorkspace?.image_url} alt={currentWorkspace?.name} className="w-8 h-8 rounded shadow" />
                     <div className="min-w-0 flex-1">
                         <p className="font-semibold text-gray-800 dark:text-white text-sm truncate">
                             {currentWorkspace?.name || "Select Workspace"}
@@ -55,20 +69,22 @@ function WorkspaceDropdown() {
                         <p className="text-xs text-gray-500 dark:text-zinc-400 uppercase tracking-wider mb-2 px-2">
                             Workspaces
                         </p>
-                        {dummyWorkspaces.map((ws) => {
-                            const membersCount = 0; // TODO
+                        {userMemberships.data!.map(({ organization }) => {
                             return (
-                                <div key={ws.name} onClick={() => onSelectWorkspace(ws.id)} className="flex items-center gap-3 p-2 cursor-pointer rounded hover:bg-gray-100 dark:hover:bg-zinc-800" >
-                                    <img src={ws.image_url} alt={ws.name} className="w-6 h-6 rounded" />
+                                <div
+                                    key={organization.id}
+                                    onClick={() => onSelectWorkspace(organization.id)} className="flex items-center gap-3 p-2 cursor-pointer rounded hover:bg-gray-100 dark:hover:bg-zinc-800"
+                                >
+                                    <img src={organization.imageUrl} alt={organization.name} className="w-6 h-6 rounded" />
                                     <div className="flex-1 min-w-0">
                                         <p className="text-sm font-medium text-gray-800 dark:text-white truncate">
-                                            {ws.name}
+                                            {organization.name}
                                         </p>
                                         <p className="text-xs text-gray-500 dark:text-zinc-400 truncate">
-                                            {membersCount | 0} members
+                                            {organization.membersCount || 0} members
                                         </p>
                                     </div>
-                                    {currentWorkspace?.id === ws.id && (
+                                    {currentWorkspace?.id === organization.id && (
                                         <Check className="w-4 h-4 text-blue-600 dark:text-blue-400 shrink-0" />
                                     )}
                                 </div>
@@ -78,7 +94,13 @@ function WorkspaceDropdown() {
 
                     <hr className="border-gray-200 dark:border-zinc-700" />
 
-                    <div className="p-2 cursor-pointer rounded group hover:bg-gray-100 dark:hover:bg-zinc-800" >
+                    <div
+                        onClick={() => {
+                            openCreateOrganization();
+                            setIsOpen(false);
+                        }}
+                        className="p-2 cursor-pointer rounded group hover:bg-gray-100 dark:hover:bg-zinc-800"
+                    >
                         <p className="flex items-center text-xs gap-2 my-1 w-full text-blue-600 dark:text-blue-400 group-hover:text-blue-500 dark:group-hover:text-blue-300">
                             <Plus className="w-4 h-4" /> Create Workspace
                         </p>
