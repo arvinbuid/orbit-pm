bc88ff749d3dd35d25770714bdabf9bde850bb80
Arvin Buid <arvinbuid01@gmail.com>
2026-02-25 12:20:30 +0800

    feat: create createTask controller function

---
+++ server/controllers/taskController.js
@@ -0,0 +1,48 @@
+import prisma from "../configs/prisma.js";
+
+// Create task
+export const createTask = async (req, res) => {
+  try {
+    const {userId} = req;
+    const {projectId, title, description, status, type, priority, assigneeId, due_date} = req.body;
+    const origin = req.get("origin");
+
+    // Check if user have admin role for project
+    const project = await prisma.project.findUnique({
+      where: {id: projectId},
+      include: {members: {include: {user: true}}},
+    });
+
+    if (!project) {
+      return res.status(404).json({message: "Project not found."});
+    } else if (project.team_lead !== userId) {
+      return res.status(403).json({message: "You do not have admin privileges for this project."});
+    } else if (assigneeId && !project.members.find((member) => member.userId === assigneeId)) {
+      return res.status(403).json({message: "Assignee is not a member of this project/workspace."});
+    }
+
+    // Create task
+    const task = await prisma.task.create({
+      data: {
+        projectId,
+        title,
+        description,
+        status,
+        type,
+        priority,
+        assigneeId,
+        due_date: new Date(due_date),
+      },
+    });
+
+    const taskWithAssignee = await prisma.task.findUnique({
+      where: {id: task.id},
+      include: {assignee: true},
+    });
+
+    res.json({task: taskWithAssignee, message: "Task created successfully."});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: error.code | error.message});
+  }
+};
