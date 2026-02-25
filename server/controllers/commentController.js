e1c2bb3fbe0469d34a26a6783e36f4226dfb2b5d
Arvin Buid <arvinbuid01@gmail.com>
2026-02-25 13:54:37 +0800

    feat: create commentController file and create createComment controller function

---
+++ server/controllers/commentController.js
@@ -0,0 +1,40 @@
+import prisma from "../configs/prisma.js";
+
+// Create comment
+export const createComment = async (req, res) => {
+  try {
+    const {userId} = req;
+    const {taskId, content} = req.body;
+
+    // Check if user is project member
+    const task = await prisma.task.findUnique({
+      where: {id: taskId},
+    });
+
+    if (!task) {
+      return res.status(404).json({message: "Task not found."});
+    }
+
+    const project = await prisma.project.findUnique({
+      where: {id: task.projectId},
+      include: {members: {include: {user: true}}},
+    });
+
+    if (!project) {
+      return res.status(404).json({message: "Project not found."});
+    } else if (!project.members.find((member) => member.userId === userId)) {
+      return res.status(403).json({message: "You are not a member of this project."});
+    }
+
+    // Create comment
+    const comment = await prisma.comment.create({
+      data: {taskId, userId, content},
+      include: {user: true},
+    });
+
+    res.json({comment});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: error.code || error.message});
+  }
+};
