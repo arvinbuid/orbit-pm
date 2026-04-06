21ffa0b7b990f14afd629d5482ea8c1f453407ec
Arvin Buid <arvinbuid01@gmail.com>
2026-04-06 15:32:46 +0800

    style: move hover and font-weight transitions into their respective elements in SignInPage and SignUpPage

--- client/src/components/auth/SignInPage.tsx
+++ client/src/components/auth/SignInPage.tsx
@@ -6,9 +6,9 @@ const SignInPage = () => {
     return (
         <div className="flex justify-center mt-12 md:mt-24 h-screen">
             <div>
-                <Link to='/' className="flex items-center gap-2 text-blue-600 hover:text-blue-700 hover:font-medium transition-all">
-                    <ArrowLeft className="w-3 h-3" />
-                    <p className="text-sm">Back to Home</p>
+                <Link to='/' className="flex items-center gap-2">
+                    <ArrowLeft className="w-3 h-3 text-blue-600 hover:text-blue-700 hover:font-medium transition-colors" />
+                    <p className="text-sm text-blue-600 hover:text-blue-700 hover:font-medium transition-colors">Back to Home</p>
                 </Link>
                 <div className="mt-4">
                     <SignIn
--- client/src/components/auth/SignUpPage.tsx
+++ client/src/components/auth/SignUpPage.tsx
@@ -6,9 +6,9 @@ const SignUpPage = () => {
     return (
         <div className="flex justify-center mt-12 md:mt-14 h-screen">
             <div>
-                <Link to='/' className="flex items-center gap-2 text-blue-600 hover:text-blue-700 hover:font-medium transition-all">
-                    <ArrowLeft className="w-3 h-3" />
-                    <p className="text-sm">Back to Home</p>
+                <Link to='/' className="flex items-center gap-2">
+                    <ArrowLeft className="w-3 h-3 text-blue-600 hover:text-blue-700 hover:font-medium transition-colors" />
+                    <p className="text-sm text-blue-600 hover:text-blue-700 hover:font-medium transition-colors">Back to Home</p>
                 </Link>
                 <div className="mt-4">
                     <SignUp
